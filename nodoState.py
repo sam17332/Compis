2613a5be1612bb5aa759595083e1b6f9c996c445
toti <sam17332@uvg.edu.gt>
2021-10-08 20:26:43 -0600

    Se crea nueva clase nodoState

---
+++ nodoState.py
@@ -0,0 +1,33 @@
+class NodoState:
+    def __init__(self):
+        self.siguiente = ""
+        self.codigo = ""
+        self.valorTrue = ""
+        self.valorFalse = ""
+
+    def getSiguiente(self):
+        return self.siguiente
+
+    def setSiguiente(self, siguiente):
+        self.siguiente = siguiente
+
+    def getCodigo(self):
+        return self.codigo
+
+    def setCodigo(self, codigo):
+        self.codigo = codigo
+
+    def getValorTrue(self):
+        return self.valorTrue
+
+    def setValorTrue(self, valorTrue):
+        self.valorTrue = valorTrue
+
+    def getValorFalse(self):
+        return self.codigo
+
+    def setValorFalse(self, valorFalse):
+        self.valorFalse = valorFalse
+
+    def getNodeState(self):
+        return [self.siguiente, self.codigo, self.valorTrue, self.valorFalse]
