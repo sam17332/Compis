1fcfe6c5baae73c3d47d10c6c4910de214d45008
toti <sam17332@uvg.edu.gt>
2021-10-08 22:26:31 -0600

    Se agrega generacion de codigo para if's y return.

--- main2.py
+++ main2.py
@@ -6,6 +6,7 @@ from DecafGramParser import DecafGramParser
 import pickle5 as pickle
 from tablas import *
 from nodo import *
+from nodoState import *
 import pprint
 import sys
 
@@ -19,6 +20,8 @@ class Proyecto2(DecafGramListener):
     def __init__(self):
         self.contNodos = 0
         self.contTemps = 0
+        self.contTrue = 0
+        self.contFalse = 0
         self.diccVariables = {}
         self.diccEstructuras = {}
         self.diccMetodos = {}
@@ -96,28 +99,29 @@ class Proyecto2(DecafGramListener):
 
         return arrayNodos
 
+    def nuevaEtiqueta(self, cond):
+        value = ''
+        if cond == 'true':
+            value = f'IF_TRUE_{self.contTrue}'
+            self.contTrue += 1
+        elif cond == 'false':
+            value = f'IF_FALSE_{self.contFalse}'
+            self.contFalse += 1
+        else:
+            value = f'{cond}:'
+
+        return value
+
     def iterateArrayProd(self):
         print()
         print("----CODIGO INTERMEDIO----")
         print()
         print()
         for prod in self.arrayProd:
-            if isinstance(prod, Nodo):
+            if isinstance(prod, Nodo) or isinstance(prod, NodoState):
                 print(prod.getCodigo())
             else:
                 print(prod)
-
-    # def enterVardeclr(self, ctx: DecafGramParser.VardeclrContext):
-    #     self.diccContext[ctx] = ctx
-
-    # def exitVardeclr(self, ctx: DecafGramParser.VardeclrContext):
-    #     self.diccContext[ctx] = self.diccContext[ctx.getChild(0)]
-
-    # def enterField_var(self, ctx: DecafGramParser.Field_varContext):
-    #     self.diccContext[ctx] = ctx
-
-    # def exitField_var(self, ctx: DecafGramParser.Field_varContext):
-    #     self.diccContext[ctx] = self.diccContext[ctx.getChild(0)]
 
     def enterLocation(self, ctx: DecafGramParser.LocationContext):
         if ctx not in self.diccContext:
@@ -125,11 +129,6 @@ class Proyecto2(DecafGramListener):
 
     def exitLocation(self, ctx: DecafGramParser.LocationContext):
         self.diccContext[ctx] = self.diccContext[ctx.getChild(0)]
-
-    # def enterVar_id(self, ctx: DecafGramParser.Var_idContext):
-    #     parent = ctx.parentCtx
-    #     if parent not in self.diccContext:
-    #         self.diccContext[ctx] = self.diccContext[parent]
 
     def exitVar_id(self, ctx: DecafGramParser.Var_idContext):
         nombre = ctx.getText()
@@ -141,6 +140,15 @@ class Proyecto2(DecafGramListener):
         nodo.setDir(self.generateDir(variable))
 
         self.diccContext[ctx] = nodo
+
+    def exitVardeclr(self, ctx: DecafGramParser.VardeclrContext):
+        self.diccContext[ctx] = self.diccContext[ctx.getChild(1)]
+
+    def exitField_var(self, ctx: DecafGramParser.Field_varContext):
+        self.diccContext[ctx] = self.diccContext[ctx.getChild(0)]
+
+    def exitBlock(self, ctx: DecafGramParser.BlockContext):
+        self.diccContext[ctx] = self.diccContext[ctx.getChild(1)]
 
     def exitExpr_presedencia1(self, ctx: DecafGramParser.Expr_presedencia1Context):
         nodo1 = self.diccContext[ctx.getChild(0)]
@@ -211,6 +219,16 @@ class Proyecto2(DecafGramListener):
         nodo.setCodigo(codigoConcat)
         self.diccContext[ctx] = nodo
 
+    def exitExpr_negacion(self, ctx: DecafGramParser.Expr_negacionContext):
+        nodo1 = self.diccContext[ctx.getChild(1)]
+        nodo = Nodo(self.contNodos)
+        self.contNodos += 1
+        temp = self.genTemp()
+        nodo.setDir(temp)
+        codigoConcat = ' ' + nodo1.getCodigo() + (nodo.getDir() + ' = NEGADO ' + nodo1.getDir()) + '\n'
+        nodo.setCodigo(codigoConcat)
+        self.diccContext[ctx] = nodo
+
     def exitExpr_parentesis(self, ctx: DecafGramParser.Expr_parentesisContext):
         nodo1 = self.diccContext[ctx.getChild(1)]
         nodo = Nodo(self.contNodos)
@@ -219,8 +237,54 @@ class Proyecto2(DecafGramListener):
         nodo.setCodigo(nodo1.getCodigo())
         self.diccContext[ctx] = nodo
 
-    # def exitExpr_literal(self, ctx: DecafGramParser.Expr_literalContext):
-    #     return super().exitExpr_literal(ctx)
+    def exitStatement_if(self, ctx: DecafGramParser.Statement_ifContext):
+        nodoState = NodoState()
+        nodoB = self.diccContext[ctx.expr()]
+        nodoS1 = self.diccContext[ctx.getChild(4)]
+
+        nodoB.setValorTrue(self.nuevaEtiqueta('true'))
+        endIf = self.nuevaEtiqueta('false')
+
+        codigConcat = nodoB.getCodigo() + (' IF ' + f't{self.contTemps-1} > 0 '  f' GOTO {nodoB.getValorTrue()}') + '\n' +\
+            (' GOTO ' + endIf) + '\n' + ' ' + self.nuevaEtiqueta(nodoB.getValorTrue()) + '\n' + ' ' + nodoS1.getCodigo() + '\n' + ' ' + self.nuevaEtiqueta(endIf)
+        nodoState.setCodigo(codigConcat)
+
+        self.diccContext[ctx] = nodoState
+        self.arrayProd.append(nodoState)
+
+
+    def exitExpr_normal(self, ctx: DecafGramParser.Expr_normalContext):
+        if ctx.eq_op is not None:
+            temp = self.genTemp()
+            nodoState = NodoState()
+            self.contNodos += 1
+            nodo = self.diccContext[ctx.getChild(0)]
+            codigoConcat = ' ' + temp + ' = ' + nodo.getDir() + ' ' + ctx.eq_op().getText() + ' ' + ctx.getChild(2).getText() + '\n'
+            nodoState.setCodigo(codigoConcat)
+            self.diccContext[ctx] = nodoState
+        else:
+            self.diccContext[ctx] = self.diccContext[ctx.getChild(0)]
+
+    def exitExpr_literal(self, ctx: DecafGramParser.Expr_literalContext):
+        self.diccContext[ctx] = self.diccContext[ctx.getChild(0)]
+
+    def exitLiteral(self, ctx: DecafGramParser.LiteralContext):
+        self.diccContext[ctx] = self.diccContext[ctx.getChild(0)]
+
+    def exitInt_literal(self, ctx: DecafGramParser.Int_literalContext):
+        nodo = Nodo(self.contNodos)
+        self.contNodos += 1
+        self.diccContext[ctx] = nodo
+
+    def exitChar_literal(self, ctx: DecafGramParser.Char_literalContext):
+        nodo = Nodo(self.contNodos)
+        self.contNodos += 1
+        self.diccContext[ctx] = nodo
+
+    def exitBool_literal(self, ctx: DecafGramParser.Bool_literalContext):
+        nodo = Nodo(self.contNodos)
+        self.contNodos += 1
+        self.diccContext[ctx] = nodo
 
     def exitStatement_assign(self, ctx: DecafGramParser.Statement_assignContext):
         nodoL = self.diccContext[ctx.location()]
@@ -236,6 +300,7 @@ class Proyecto2(DecafGramListener):
         nodo.setCodigo(codigoConcat)
 
         self.diccContext[ctx] = nodo
+        # TODO verificar cuando hay que hacer append y cuando no
         self.arrayProd.append(nodo)
 
     def exitStatement_return(self, ctx: DecafGramParser.Statement_returnContext):
